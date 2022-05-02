17bbe6d075459cabc2aab9a451c4cff7791d2c97
Robert Behm <matthewrobertbehm@gmail.com>
2022-05-02 15:41:36 -0400

    made some changes

--- src/screens/Cartscreen.js
+++ src/screens/Cartscreen.js
@@ -36,8 +36,8 @@ export default function Cartscreen() {
                       {item.name} [{item.size}]
                     </h5>
                     <h5>
-                      Price : {item.quantity} * {item.prices[0][item.size] / 10}{" "}
-                      = ${(item.quantity * item.prices[0][item.size]) / 10}
+                      Price : {item.quantity} * {item.prices[0][item.size]} = $
+                      {item.quantity * item.prices[0][item.size]}
                     </h5>
                     <h5 style={{ display: "inline" }}>Quantity : </h5>
                     <i
